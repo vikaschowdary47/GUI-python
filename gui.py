aa982f148dc292458a591ca02d66c7cc7bbf6dd6
vikascowdary47 <vikaschowdary47@gmail.com>
2020-08-27 20:14:06 +0530

    open files

--- gui.py
+++ gui.py
@@ -4,6 +4,11 @@ import os
 
 root = tk.Tk()
 
+
+def addApp:
+    filename = filedialog.askopenfilename(initialdir='/', title='Select File', filetypes(('executables', '*.exe'), ('all files', *.*)))
+
+
 canvas = tk.Canvas(root, height=700, width=700, bg='blue')
 canvas.pack()
 
@@ -11,7 +16,7 @@ frame = tk.Frame(root, bg='white')
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 openFile = tk.Button(root, text='Open File', padx=10,
-                     pady=5, fg='white', bg='green')
+                     pady=5, fg='white', bg='green', command=addApp)
 openFile.pack()
 
 openApp = openFile = tk.Button(root, text='Open App', padx=10,
