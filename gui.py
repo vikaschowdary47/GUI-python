11bd203346eec226cd7e3089f767fa932deb4638
vikascowdary47 <vikaschowdary47@gmail.com>
2020-08-27 20:00:13 +0530

    button

--- gui.py
+++ gui.py
@@ -10,4 +10,8 @@ canvas.pack()
 frame = tk.Frame(root, bg='white')
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
+openFile = tk.Button(root, text='Open File', padx=10,
+                     pady=5, fg='white', bg='green')
+openFile.pack()
+
 root.mainloop()
